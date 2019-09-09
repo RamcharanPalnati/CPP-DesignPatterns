e51cd1ef9b4de32d749396218629a0d99540a4fe
ramcharan,palnati <ramcharan.palnati@opsramp.com>
2019-09-09 11:24:07 +0530

    namesapce

--- namespaces/namespace.h
+++ namespaces/namespace.h
@@ -1,20 +1,22 @@
 #include <iostream>
 
-namespace nameSpace{
-    class sampleClass{
-        public:
-            sampleClass();
-            void printClass();
+namespace nameSpace
+{
+class sampleClass
+{
+public:
+    sampleClass();
+    void printClass();
+};
+} // namespace nameSpace
 
-    };    
-}
 
-
-namespace nameSpace1{
-    class sampleClass{
-        public:
-            sampleClass();
-            void printClass();
-
-    };    
-}
+namespace nameSpace1
+{
+class sampleClass
+{
+public:
+    sampleClass();
+    void printClass();
+};
+} // namespace nameSpace1
